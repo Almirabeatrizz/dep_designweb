82810dab2e1df57e97e84ee2d957a0fe856e3dd1
Almira Beatriz <almirabeatriz.12@outlook.com>
2026-08-26 15:58:45 -0300

    sintexe js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-alert("oi do externo");
+alert("oi do externo"); 
+
+var num = parseFloat(prompt("Digite um valor"));
+if (num % 2 == 0){
+    alert ('${num} é par');
+}else if (num %2 != 0){
+    alert(`${num} é ímpar`);
+}else{
+    alert(`${num} é zero`);
+}
+
+alert("Fim");
