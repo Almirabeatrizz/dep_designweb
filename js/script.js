399859e25945d9ed28aa7c5b97d5d8b3a08d1963
Almira Beatriz <almirabeatriz.12@outlook.com>
2026-08-26 16:16:40 -0300

    atv

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
-alert("oi do externo"); 
-
-var num = parseFloat(prompt("Digite um valor"));
-if (num % 2 == 0){
-    alert ('${num} é par');
-}else if (num %2 != 0){
-    alert(`${num} é ímpar`);
+//Declaro a constante viva como true 
+const viva = confirm ("Clique em ok se voce esta viva");
+//Crio um bloco de execucao que so é executado se viva igual a true 
+if (viva){
+    //Declaro a variavel nasc e atribuo a ela o valor digitado como int
+    var nasc = parseFloat(prompt("Digite seu ano de nasciemento:"));
+    //Declaro a variavel nome e atribuo a ela o valor digitado (padrao str)
+    let nome = parseFloat(prompt("Digite seu nome: "));
+    //Declaro a variavel altura e atribuo a ela o valor digitado como float
+    let altura = parseFloat(prompt("Digite a sua altura em metros: "))
+    //Atribuo o valor de idade igual ao resultado da expressao
+    idade = 2026 - nasc;
+    //Exibo msg com o nome digitado e a idade calculada 
+    alert(`${nome} voce tem ${idade} anos`);
 }else{
-    alert(`${num} é zero`);
+    //Exibo msg dizendo que a pessoa esta morta
+    alert(`voce esta morta`);
 }
 
-alert("Fim");
+
+
