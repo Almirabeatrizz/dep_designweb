50734335fa85d236090d07ef0c0e66c5eb10e32c
Almira Beatriz <almirabeatriz.12@outlook.com>
2026-09-02 14:22:38 -0300

    atv2

---
+++ scripti.js
@@ -0,0 +1,27 @@
+//Declaro a variável (crio) e atribuo nome
+var nome = "Almira"
+//Declaro a variável (crio) e atribuo nasc
+let nasc = 2007;
+//Declaro a variável (crio) e atribuo viva
+const viva = confirm ("Clique em ok, se estiver viva")
+
+//Declaro a variável altura;
+let altura;
+
+function calcIdade(ano = 2026){
+    let idade = ano - nasc;
+    console.log(`idade dentro da funcao: ${idade}`)
+    return idade;
+}
+
+alert(`Idade fora da funcao: ${calcIdade()}`);
+/*
+Da erro pois idade nao existe fora do escopo dela (bloco calcIdade)
+console.log(`idade fora da funcao: ${idade}`)
+*/
+
+if(viva){
+    //Atribui valor em viva 
+    altura = prompt("Digite a sua altura");
+}
+
