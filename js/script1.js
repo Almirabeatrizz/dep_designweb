08ac9d507e5f7de87a983f991037ae1a38431629
Almira Beatriz <almirabeatriz.12@outlook.com>
2026-09-16 13:48:34 -0300

    atv4

---
+++ js/script1.js
@@ -0,0 +1,16 @@
+function saudacao_a(){
+    alert(`Ola Fulano, seja bem-vindo!`);
+}
+
+function saudacao_b(){
+    alert(`Olá ${nome}, seja bem-vindo!`);
+}
+
+function saudacao_c(nome="Desconhecido"){
+    alert(`Olá ${nome}, seja bem vindo!`)
+}
+
+function suadacao_d(nome="Desconhecido"){
+    return `Olá ${nome}, seja bem-vindo!`;
+}
+
